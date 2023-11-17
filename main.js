c2eb5548fcae90054613be8ee7ff0d77168a5a8f
Todd47 <swingtodd@hotmail.com>
2023-11-17 08:42:13 +0800

    init commit

---
+++ main.js
@@ -0,0 +1,35 @@
+if (ToddsMod === undefined) var ToddsMod = {
+    name: 'ToddsMod',
+
+    register: function() {
+        Game.registerMod(this.name, this);
+    },
+
+
+
+};
+if (typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+
+ToddsMod.launch = function () {
+    this.register()
+    Game.Notify(`Cooler mod loaded!`,[16,5]);
+
+    /**
+
+     All the code you want to delay goes here
+     Put "ToddsMod.isLoaded = 1;" somewhere within
+
+     **/
+    ToddsMod.isLoaded = 1;
+}
+
+// Load mod
+if (!ToddsMod.isLoaded) {
+    if (CCSE && CCSE.isLoaded) {
+        ToddsMod.launch();
+    } else {
+        if (!CCSE) var CCSE = {};
+        if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+        CCSE.postLoadHooks.push(ToddsMod.launch);
+    }
+}
